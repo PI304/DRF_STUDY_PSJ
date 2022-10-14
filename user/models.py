d276849ec43b1f4b6bcef95e9723d025f72787ab
june4164 <june4164@naver.com>
2022-10-15 06:49:14 +0900

    2nd commit

--- user/models.py
+++ user/models.py
@@ -6,29 +6,25 @@ from django.contrib.auth.models import (
 
 class UserManager(BaseUserManager):
     def create_user(
-        self,
-        email,
-        password=None)
-        :
-        """
-        Creates and saves a User with the given email, date of
-        birth and password.
-        """
+            self,
+            email=None,
+            password=None
+        ):
+
         if not email:
             raise ValueError('Users must have an email address')
 
-        user = self.model(
-            email=self.normalize_email(email),
-            date_of_birth=date_of_birth,
-        )
+        user = self.model(email=self.normalize_email(email))
 
         user.set_password(password)
         user.save(using=self._db)
         return user
 
-    def create_superuser(self,
-    email,
-    password=None):
+    def create_superuser(
+            self,
+            email,
+            password=None
+        ):
         """
         Creates and saves a superuser with the given email, date of
         birth and password.
@@ -36,7 +32,6 @@ class UserManager(BaseUserManager):
         user = self.create_user(
             email,
             password=password,
-            date_of_birth=date_of_birth,
         )
         user.is_admin = True
         user.save(using=self._db)
@@ -44,19 +39,50 @@ class UserManager(BaseUserManager):
 
 
 class MyUser(AbstractBaseUser):
+    id = models.BigAutoField(
+        primary_key=True
+        null=False
+        ) 
+    #id-유저번호/bigautofield 1~~ int/primary_key = user 클래스의 첫 밸류
+    
     email = models.EmailField(
         verbose_name='email address',
         max_length=255,
         unique=True,
+        null=False
     )
-    date_of_birth = models.DateField()
-    is_active = models.BooleanField(default=True)
+
+    nickname = models.CharField(
+        max_length=20,
+        null=False,
+        blank=False
+    )
+    
+    profile_image_url = models.URLField(
+        max_length=512,
+        blank=True,
+        default="https://hermes4164.web.app/assets/img/%EC%9C%A0%EB%A0%B9.jpg", #본인 팩맨 유령 프사, 추후 변경
+        null=True
+    )
+    
+    profile_message = models.CharField(
+        max_length=200,
+        blank=True,
+        null=True
+    )
+
+    is_active = models.BooleanField(
+        default=True
+        null=False
+        )
+    
     is_admin = models.BooleanField(default=False)
 
     objects = MyUserManager()
 
+    EMAIL_FIELD = 'email'
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['date_of_birth']
+    REQUIRED_FIELDS = ['nickname']  #email, 비밀번호, 닉네임은 필수지만 user를 생성할때 이미 email과 비밀번호는 명시
 
     def __str__(self):
         return self.email
@@ -76,3 +102,10 @@ class MyUser(AbstractBaseUser):
         "Is the user a member of staff?"
         # Simplest possible answer: All admins are staff
         return self.is_admin
+
+"""
+참조
+https://docs.djangoproject.com/en/3.2/topics/auth/customizing/#substituting-a-custom-user-model
+https://docs.djangoproject.com/en/3.2/ref/models/fields/#model-field-types
+https://docs.djangoproject.com/en/3.2/topics/db/models/#field-types
+"""
